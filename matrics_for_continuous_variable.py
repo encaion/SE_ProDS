9da51c70d8b2311dda3ca53254eb639763d5bebd
Seung Wook Kim <CEO@Rloha.io>
2019-05-17 11:24:37 +0900

    Create matrics_for_continuous_variable.py

---
+++ matrics_for_continuous_variable.py
@@ -0,0 +1,7 @@
+from sklearn import metrics
+import numpy as np
+metrics.mean_absolute_error() # MAE
+metrics.mean_absolute_error() * 100 # MAPE
+metrics.mean_squared_error() # MSE
+metrics.mean_squared_error() ** 0.5 # RMSE
+np.sqrt(metrics.mean_squared_error() # RMSE
