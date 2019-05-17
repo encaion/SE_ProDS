f256a73007a050e8f3d509be9d0764a652871bc0
Seung Wook Kim <CEO@Rloha.io>
2019-05-17 11:27:16 +0900

    Update matrics_for_continuous_variable.py

--- matrics_for_continuous_variable.py
+++ matrics_for_continuous_variable.py
@@ -1,7 +1,9 @@
 from sklearn import metrics
 import numpy as np
-metrics.mean_absolute_error() # MAE
-metrics.mean_absolute_error() * 100 # MAPE
-metrics.mean_squared_error() # MSE
-metrics.mean_squared_error() ** 0.5 # RMSE
-np.sqrt(metrics.mean_squared_error() # RMSE
+y_true = [3, 0.0, 2, 7]
+y_pred = [2.5, -0.3, 2, 8]
+metrics.mean_absolute_error(y_true, y_pred) # MAE
+metrics.mean_absolute_error(y_true, y_pred) * 100 # MAPE
+metrics.mean_squared_error(y_true, y_pred) # MSE
+metrics.mean_squared_error(y_true, y_pred) ** 0.5 # RMSE
+np.sqrt(metrics.mean_squared_error(y_true, y_pred)) # RMSE
